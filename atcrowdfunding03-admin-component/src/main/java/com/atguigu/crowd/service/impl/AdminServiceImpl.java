725e575c915488cc0453098a1a962e7991704d5c
Dragon21-ux <65547242+Dragon21-ux@users.noreply.github.com>
2021-10-20 23:54:05 +0800

    PageHelper plugins

--- atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/service/impl/AdminServiceImpl.java
+++ atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/service/impl/AdminServiceImpl.java
@@ -7,6 +7,8 @@ import com.atguigu.crowd.exception.LoginFaildException;
 import com.atguigu.crowd.mapper.AdminMapper;
 import com.atguigu.crowd.service.api.AdminService;
 import com.atguigu.crowd.util.CrowdUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -63,6 +65,16 @@ public class AdminServiceImpl implements AdminService {
         }
         // 8.如果一致则返回Admin对象
         return admin;
+    }
+
+    @Override
+    public PageInfo<Admin> getPageInfo(String keyword, Integer pageNum, Integer pageSize) {
+        //1、调用PageHelper的静态方法，开启分页功能
+        //  这充分体现了PageHelper的“非侵入式”设计：原本要做的查询不必有任何修改
+        PageHelper.startPage(pageNum,pageSize);
+        //2、执行查询
+        List<Admin> adminList = adminMapper.selectAdminListByKeyword(keyword);
+        return new PageInfo<Admin>(adminList);
     }
 
 }
